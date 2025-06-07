f88669cacc2ffb18680f8ba12b34973f8ed5655a
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-06-08 00:46:47 +0530

    feat: add authentication and super admin route handlers for user management

---
+++ src/Presentation/routes/superAdminRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import Container from "typedi";
+import { SuperAdminController } from "../controllers/super-admin/superAdminController";
+
+
+const superAdminRoutes = Router();
+const superController = Container.get(SuperAdminController);
+
+superAdminRoutes.post('/create-admin', superController.createAdminHandler);
+superAdminRoutes.post('/update-role', superController.updateUserRoleHandler);
+
+export default superAdminRoutes;
