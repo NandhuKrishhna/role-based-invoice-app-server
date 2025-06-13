824b41862d90b84231250554669007faa1900a7c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-06-13 22:52:12 +0530

    feat: update IUserRepository interface to improve user role handling and add user retrieval methods

--- src/Domain/repositories/IUserRepository.ts
+++ src/Domain/repositories/IUserRepository.ts
@@ -1,13 +1,31 @@
 import { Token } from "typedi";
 import { UserDocument } from "../../Infrastructure/Database/Models/usermodel";
-import { IAdminDataParams } from "../types/admin.types";
-import mongoose from "mongoose";
-
+import { IAdminDataParams, IGetAllUsersParams } from "../types/admin.types";
+import Role from "../../Shared/constants/roles";
+import { GetAllUsersResponse } from "../../Infrastructure/Database/repositories/user.repository";
+interface IUserNode {
+    id: string;
+    name: string;
+    email: string;
+    role: Role;
+    children?: IUserNode[];
+}
 export interface IUserRepository {
     findUserByEmail(email: string): Promise<UserDocument | null>;
     createAdmin(adminData: IAdminDataParams): Promise<UserDocument>;
-    findUserById(userId: mongoose.Types.ObjectId): Promise<UserDocument | null>;
-    updateUserRole(targetUserId: mongoose.Types.ObjectId, role: string): Promise<UserDocument>;
+    findUserById(userId: string): Promise<UserDocument | null>;
+    updateUserRole(targetUserId: string, role: string): Promise<UserDocument>;
+    deleteUserById(userId: string): Promise<void>;
+    getAllUsers(filterOptions: IGetAllUsersParams): Promise<{
+        users: UserDocument[];
+        total: number;
+        page: number;
+        limit: number;
+        totalPages: number;
+    }>;
+    findUsersByCreatorId(creatorId: string): Promise<UserDocument | null>;
+    getAllUsersForAdmin(user: UserDocument, filter: IGetAllUsersParams): Promise<any>
+
 }
 
 export const IUserRepositoryToken = new Token<IUserRepository>();
