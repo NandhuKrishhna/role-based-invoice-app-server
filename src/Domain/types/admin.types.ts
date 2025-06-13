18b3c505f08149cec396e0ec9160760850d75d4d
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-06-13 22:52:18 +0530

    feat: update admin types to use string for IDs and enhance user retrieval parameters

--- src/Domain/types/admin.types.ts
+++ src/Domain/types/admin.types.ts
@@ -1,18 +1,38 @@
 import mongoose from "mongoose";
 import Role from "../../Shared/constants/roles";
+import { UserDocument } from "../../Infrastructure/Database/Models/usermodel";
 
 export interface IAdminDataParams {
+    _id?: string;
     name: string;
     email: string;
     password: string;
     role: string;
     group?: string;
-    createdBy?: mongoose.Types.ObjectId;
+    createdBy?: String;
 }
 
 
 export interface IUpdateUserRoleParams {
-    targetUserId: mongoose.Types.ObjectId,
+    targetUserId: string,
     role: Role;
-    userId: mongoose.Types.ObjectId;
+    userId: string;
+};
+
+export interface IGetAllUsersParams {
+    page?: number;
+    limit?: number;
+    search?: string;
+    role?: Role;
+    status?: "blocked" | "active";
+    group?: string;
+    sortBy?: "name" | "email" | "role" | "status";
+    sortOrder?: "asc" | "desc";
+    createdBy?: string;
+}
+
+export interface IGetAllUser {
+    filterOptions: IGetAllUsersParams;
+    role: string,
+    userId: string
 }
