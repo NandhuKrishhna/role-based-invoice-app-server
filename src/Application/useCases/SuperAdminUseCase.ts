024b1ecad9db34644e7192892800671bc9d20267
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-06-13 22:51:58 +0530

    feat: enhance SuperAdminUseCase with user role management and retrieval services

--- src/Application/useCases/SuperAdminUseCase.ts
+++ src/Application/useCases/SuperAdminUseCase.ts
@@ -1,29 +1,33 @@
 import { Inject, Service } from "typedi";
-import { IAdminDataParams, IUpdateUserRoleParams } from "../../Domain/types/admin.types";
+import { IAdminDataParams, IGetAllUsersParams, IUpdateUserRoleParams } from "../../Domain/types/admin.types";
 import { IUserRepository, IUserRepositoryToken } from "../../Domain/repositories/IUserRepository";
 import appAssert from "../../Shared/utils/appAssert";
 import { CONFLICT } from "../../Shared/constants/http";
 import { ERROR_MESSAGES } from "../../Shared/constants/messages";
-import mongoose from "mongoose";
 import Role from "../../Shared/constants/roles";
+import generateUserId from "../../Infrastructure/Database/repositories/CounterSchema";
 
 @Service()
 export class SuperAdminUseCase {
     constructor(
         @Inject(IUserRepositoryToken) private __userRepository: IUserRepository
     ) { }
-    async createAdminService(adminData: IAdminDataParams, userId: mongoose.Types.ObjectId): Promise<any> {
+    async createAdminService(adminData: IAdminDataParams, userId: string, creatorRole: string): Promise<any> {
         const isAdminExists = await this.__userRepository.findUserByEmail(adminData.email);
         appAssert(!isAdminExists, CONFLICT, ERROR_MESSAGES.EMAIL_ALREADY_EXISTS);
 
+        const generatedId = await generateUserId(adminData.role as Role);
+
         const admin = await this.__userRepository.createAdmin({
+            _id: generatedId,
             name: adminData.name,
             email: adminData.email,
             password: adminData.password,
             role: adminData.role,
             group: adminData.group,
             createdBy: userId
-        })
+        });
+        appAssert(admin, CONFLICT, ERROR_MESSAGES.ADMIN_CREATION_FAILED);
         return {
             admin: admin.omitPassword(),
         }
@@ -32,7 +36,7 @@ export class SuperAdminUseCase {
 
     async updateUserRoleService({ targetUserId, role, userId }: IUpdateUserRoleParams): Promise<any> {
         const isSuperAdmin = await this.__userRepository.findUserById(userId);
-        appAssert(isSuperAdmin?.role === Role.SUPER_ADMIN, CONFLICT, ERROR_MESSAGES.NOT_AUTHORIZED);
+        appAssert(isSuperAdmin?.role !== Role.SUPER_ADMIN, CONFLICT, ERROR_MESSAGES.NOT_AUTHORIZED);
         const isTargetUser = await this.__userRepository.findUserById(targetUserId);
         appAssert(isTargetUser, CONFLICT, ERROR_MESSAGES.USER_NOT_FOUND);
 
@@ -42,5 +46,19 @@ export class SuperAdminUseCase {
         return {
             updatedUser: updatedUser.omitPassword(),
         }
+    }
+
+    async deleteUserService(targetUserId: string): Promise<void> {
+        const user = await this.__userRepository.findUserById(targetUserId);
+        appAssert(user, CONFLICT, ERROR_MESSAGES.USER_NOT_FOUND);
+        appAssert(user.role !== Role.SUPER_ADMIN, CONFLICT, ERROR_MESSAGES.CANNOT_DELETE_SUPER_ADMIN);
+
+        await this.__userRepository.deleteUserById(targetUserId);
+    };
+
+    async getAllUsersService(filterOptions: IGetAllUsersParams): Promise<any> {
+        const users = await this.__userRepository.getAllUsers(filterOptions);
+        return users;
+
     }
 }
